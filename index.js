be8a7437d132907f302eaa6572a54424c75f9f5f
Tasmia-Ishika <ishika.tasmia3787@gmail.com>
2023-11-16 20:50:43 +0600

    second commit

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 // MongoDB Connection
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zxlt1.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -28,19 +28,45 @@ async function run() {
 
     const menuCollection = client.db("bistroDb").collection("menu");
     const reviewCollection = client.db("bistroDb").collection("reviews");
+    const cartCollection = client.db("bistroDb").collection("carts");
+
+
+  // menu collection
     app.get('/menu', async(req, res) =>{
         const result = await menuCollection.find().toArray();
         res.send(result);
   })
-
-
+  // review collection
     app.get('/reviews', async(req, res) =>{
-        const result = await reviewCollection.find().toArray();
-        res.send(result);
+          const result = await reviewCollection.find().toArray();
+          res.send(result);
   })
-  
 
+  // cart collection apis
+  app.get('/carts', async (req, res) => {
+    const email = req.query.email;
+   
+    if(!email){
+      res.send([]);
+    }
+    const query = {email : email};
+    const result = await cartCollection.find(query).toArray();
+    res.send(result);
+  }) 
 
+  app.post('/carts', async(req, res) => {
+  const item = req.body;
+  console.log(item); 
+  const result = await cartCollection.insertOne(item);
+  res.send(result);
+  })
+
+  app.delete('/carts/:id', async (req, res) => {
+    const id = req.params.id;
+    const query = { _id: new ObjectId(id) };
+    const result = await cartCollection.deleteOne(query);
+    res.send(result);
+  })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
